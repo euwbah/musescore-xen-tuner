056ceecf107e3f8bb309ed2fe9a216ec3f1b3c02
euwbah <euwbah@gmail.com>
2023-02-13 04:03:10 +0800

    add 540 edo HEWM

--- tunings/hewm/generate-temperament.py
+++ tunings/hewm/generate-temperament.py
@@ -1,8 +1,8 @@
 import math
 
 PRIME_LIMIT = 61 # The highest prime used in the TUNING_CONFIG text below
-EDO = 311
-OUT_FILE_NAME = "311edo.txt"
+EDO = 540
+OUT_FILE_NAME = "540edo.txt"
 
 list_of_primes = []
 
